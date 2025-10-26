c8413bbe3f2fb6b0b44d8376517a132406489c36
val-nvs <153785603+val-nvs@users.noreply.github.com>
2025-10-26 15:29:41 +0000

    edit

--- shared-header.js
+++ shared-header.js
@@ -17,7 +17,7 @@ function createHeader() {
         <li><a href="eventos.html">Eventos</a></li>
         <li><a href="biblioteca.html">Biblioteca</a></li>
         <li><a href="cinemateca.html">Cinemateca</a></li>
-        <li><a href="cinemateca2.html">Cinemateca2</a></li>
+        <li><a href="/cinemateca/index.html">Cinemateca2</a></li>
         <li><a href="mercado.html">Mercado</a></li>
         <li><a href="comunidad.html">Comunidad</a></li>
         <li><a href="contactos.html">Contactos</a></li>
