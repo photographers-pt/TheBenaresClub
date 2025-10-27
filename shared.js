878dc432dd859fe9d529c9df87f5a320ceca5978
val-nvs <153785603+val-nvs@users.noreply.github.com>
2025-10-27 22:29:41 +0000

    edit

--- shared.js
+++ shared.js
@@ -17,7 +17,7 @@ function createHeader() {
     
     <nav aria-label="Menu principal" id="main-nav" class="active">
       <ul>
-        <li><a href="/eventos.html">Eventos</a></li>
+        <li><a href="/eventos/index.html">Eventos</a></li>
         <li><a href="/biblioteca/index.html">Biblioteca</a></li>
         <li><a href="/cinemateca-tv.html">TV</a></li>
         <li><a href="/pc.html">PC</a></li>
